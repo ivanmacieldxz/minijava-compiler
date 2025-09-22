85bb895953d6931178b8f68c5ac558aa5d04dec7
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2025-09-21 23:53:21 -0300

    Added toString implementation to TokenType enum

--- src/utils/TokenType.kt
+++ src/utils/TokenType.kt
@@ -1,6 +1,5 @@
 package utils
 
-import utils.SyntacticStackable
 
 enum class TokenType: SyntacticStackable {
 
@@ -55,5 +54,61 @@ enum class TokenType: SyntacticStackable {
     NULL,
     TRUE,
     FALSE,
+    ;
 
+    override fun toString(): String =
+        when (this) {
+            EOF -> "end of file"
+            LEFT_BRACKET -> "("
+            RIGHT_BRACKET -> ")"
+            LEFT_CURLY_BRACKET -> "{"
+            RIGHT_CURLY_BRACKET -> "}"
+            SEMICOLON -> ";"
+            COMMA -> ","
+            COLON -> ":"
+            DOT -> "."
+            MODULUS -> "%"
+            MULTIPLICATION -> "*"
+            DIVISION -> "/"
+            CLASS_IDENTIFIER -> "identificador de clase"
+            MET_VAR_IDENTIFIER -> "identificador de método o variable"
+            INTEGER_LITERAL -> "literal entero"
+            CHAR_LITERAL -> "literal char"
+            STRING_LITERAL -> "literal string"
+            GREATER_THAN -> ">"
+            GREATER_THAN_OR_EQUAL -> ">="
+            LESS_THAN -> "<"
+            LESS_THAN_OR_EQUAL -> "<="
+            ASSIGNMENT -> "="
+            EQUALS -> "=="
+            DIFFERENT -> "!="
+            NOT -> "!"
+            AND -> "&&"
+            OR -> "||"
+            ADDITION -> "+"
+            INCREMENT -> "++"
+            SUBSTRACTION -> "-"
+            DECREMENT -> "--"
+            EXTENDS -> "extends"
+            CLASS -> "class"
+            PUBLIC -> "public"
+            STATIC -> "static"
+            VOID -> "void"
+            BOOLEAN -> "boolean"
+            CHAR -> "char"
+            INT -> "int"
+            ABSTRACT -> "abstract"
+            FINAL -> "final"
+            IF -> "if"
+            ELSE -> "else"
+            WHILE -> "while"
+            RETURN -> "return"
+            VAR -> "var"
+            THIS -> "this"
+            NEW -> "new"
+            NULL -> "null"
+            TRUE -> "true"
+            FALSE -> "false"
+        }
+    
 }
